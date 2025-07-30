c74e6e220776f2e71efe949daa8019cfba95173f
Sam <122852783+Sam-Sam01@users.noreply.github.com>
2025-07-30 20:52:51 +0600

    Learning about comment

---
+++ 2. Comment/main.py
@@ -0,0 +1,7 @@
+# this is a comment.
+print("Hello World")
+# comment doesn't show in the terminal.
+# comment is for only developers note.
+'''This is multiline
+comment.'''
+print("This will show on the terminal.")
