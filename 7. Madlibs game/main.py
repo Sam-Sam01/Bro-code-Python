55fe607b36950b85c487d981f5013b54150b774c
Sam <122852783+Sam-Sam01@users.noreply.github.com>
2025-08-02 20:31:52 +0600

    Add files via upload

---
+++ 7. Madlibs game/main.py
@@ -0,0 +1,18 @@
+#Madlibs Game
+# word game where you create a story by filling in blanks with random words
+
+print(f"Today I went to a ____ zoo.")
+print(f"In an exhibit, I saw a ____.")
+print(f"____ was ____ and ____.")
+print(f"I was ____.")
+
+adj1 = input("Adjective: ")
+noun1 = input("Noun: ")
+adj2 = input("Adjective: ")
+verb1 = input("Verb: ")
+adj3 = input("Adjective: ")
+
+print(f"Today I went to a {adj1} zoo.")
+print(f"In an exhibit, I saw a{noun1}.")
+print(f"{noun1} was {adj2} and {verb1}.")
+print(f"I was {adj3}.")
