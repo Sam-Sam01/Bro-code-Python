cf4a1b6ff26a825c788ec26810794055dfd92dfe
Sam <122852783+Sam-Sam01@users.noreply.github.com>
2025-08-07 01:11:00 +0600

    Add files via upload

---
+++ 26. Shopping Cart program/main.py
@@ -0,0 +1,25 @@
+#shopping Cart program
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of {food}: "))
+        foods.append(food)
+        prices.append(price)
+
+print("-----Receipt-----")
+for food in foods:
+    print(food, end=" ")
+
+for price in prices:
+    total += price
+    
+print()
+print(f"Total: ${total}")
+print("------------------")
