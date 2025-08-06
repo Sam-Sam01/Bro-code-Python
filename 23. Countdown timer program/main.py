9e2444d151988b0eb3e3618c7bfbe94308f71757
Sam <122852783+Sam-Sam01@users.noreply.github.com>
2025-08-06 11:15:40 +0600

    Add files via upload

---
+++ 23. Countdown timer program/main.py
@@ -0,0 +1,31 @@
+import time
+
+""" my_time = int(input("enter the time in seconds: "))
+
+for x in range(0, my_time):
+    print(x)
+    time.sleep(1)
+
+print("Time's UP!\n") """
+
+'''for x in reversed(range(0, my_time)):
+        print(x)
+        time.sleep(1)
+
+print("Time's UP!")'''
+
+'''for x in range(my_time, 0, -1):
+    print(x)
+    time.sleep(1)
+
+print("Time's UP!")'''
+
+my_time = int(input("enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x%60
+    minutes = int(x/60) % 60
+    hours = int(x/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("Time's UP!") 
